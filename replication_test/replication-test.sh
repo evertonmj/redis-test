5c61a3a857250c28f10ab45165fe618b160d8d6c
Everton Mendonca Jesus <evertonmj@gmail.com>
2025-03-01 23:27:31 -0300

    refact: add dbs placeholders

--- replication_test/replication-test.sh
+++ replication_test/replication-test.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# 1. Create databases
-# apk update && apk upgrade 
-# apk add --update alpine-sdk
-# apk add --no-cache bash git openssh make cmake
-
-# wget http://download.redis.io/redis-stable.tar.gz
-# tar xvzf redis-stable.tar.gz
-# cd redis-stable
-
-# make redis-cli
-# sudo cp src/redis-cli /usr/local/bin/
-# source: ./src/redis-cli -u redis://redis-19735.re-cluster1.ps-redislabs.org:19735
-#     SET test:tb:1 "test ok"
-# destination:    src/redis-cli -u redis://redis-11027.re-cluster1.ps-redislabs.org:11027
-#     GET test:tb:1
-
-# databases removed
-SOURCE_DB=redis://redis-12720.re-cluster1.ps-redislabs.org:12720
-REPLICA_DB=redis://redis-18151.re-cluster1.ps-redislabs.org:18151
+SOURCE_DB=redis://<SOURCE_DATABASE_HOST>:<SOURCE_DATABASE_PORT>
+REPLICA_DB=redis://<REPLICA_DATABASE_HOST>:<REPLICA_DATABASE_PORT>
 
 echo "Starting input source databases..."
 
